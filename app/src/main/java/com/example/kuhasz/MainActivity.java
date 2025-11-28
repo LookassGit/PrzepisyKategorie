f3f79c780c9634d8ad3b8a999ffd009da3b5f6d4
LookassGit <lukaszszkolakonto@gmail.com>
2025-11-28 08:14:45 +0100

    klikniecie elementu listy

--- app/src/main/java/com/example/kuhasz/MainActivity.java
+++ app/src/main/java/com/example/kuhasz/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.kuhasz;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +14,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView listViewKategorie;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +24,14 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        listViewKategorie = findViewById(R.id.ListaKategorie);
+        listViewKategorie.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                view.setBackgroundColor(Color.GRAY);
+                Toast.makeText(MainActivity.this, "Kliknieto element +1", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
